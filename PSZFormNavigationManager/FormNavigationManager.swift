74bc51281e0fcd8e35090867ef2d0f0e4821ba9a
Paweł Sporysz <pawel.s@untitledkingdom.co>
2014-12-24 12:42:01 +0100

    Add project

---
+++ PSZFormNavigationManager/FormNavigationManager.swift
@@ -0,0 +1,13 @@
+//
+//  FormNavigationManager.swift
+//  PSZFormNavigationManager
+//
+//  Created by Paweł Sporysz on 24.12.2014.
+//  Copyright (c) 2014 Paweł Sporysz. All rights reserved.
+//
+
+import UIKit
+
+public class FormNavigationManager: NSObject {
+   
+}
---
+++ PSZFormNavigationManager/PSZFormNavigationManager.h
@@ -0,0 +1,19 @@
+//
+//  PSZFormNavigationManager.h
+//  PSZFormNavigationManager
+//
+//  Created by Paweł Sporysz on 24.12.2014.
+//  Copyright (c) 2014 Paweł Sporysz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSZFormNavigationManager.
+FOUNDATION_EXPORT double PSZFormNavigationManagerVersionNumber;
+
+//! Project version string for PSZFormNavigationManager.
+FOUNDATION_EXPORT const unsigned char PSZFormNavigationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSZFormNavigationManager/PublicHeader.h>
+
+
