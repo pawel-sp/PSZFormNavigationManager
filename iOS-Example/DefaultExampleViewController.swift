754e1cc8f93fc206c5750873be210cfc09406e0a
Paweł Sporysz <pawel.s@untitledkingdom.co>
2014-12-25 18:47:52 +0100

    Improve storyboard for default example

---
+++ iOS-Example/DefaultExampleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DefaultExampleViewController.swift
+//  iOS-Example
+//
+//  Created by Paweł Sporysz on 25.12.2014.
+//  Copyright (c) 2014 Paweł Sporysz. All rights reserved.
+//
+
+import UIKit
+
+class DefaultExampleViewController: UIViewController {
+
+
+
+}
